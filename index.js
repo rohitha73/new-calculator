a58842e3e23112f25185338f3e43d01418798ebc
rohitha73 <90081694+rohitha73@users.noreply.github.com>
2025-01-11 18:48:43 +0530

    added index.js
    added the button behaviour

---
+++ index.js
@@ -0,0 +1,68 @@
+
+document.querySelector('.one').addEventListener('click', function(event) {
+    console.log(event.target);
+    document.getElementById('display').innerHTML = event.target.innerHTML;
+});
+document.querySelector('.two').addEventListener('click', function(event) {
+    console.log(event.target);
+    document.getElementById('display').innerHTML = event.target.innerHTML;
+});
+document.querySelector('.three').addEventListener('click', function(event) {
+    console.log(event.target);
+    document.getElementById('display').innerHTML = event.target.innerHTML;
+});
+document.querySelector('.four').addEventListener('click', function(event) {
+    console.log(event.target);
+    document.getElementById('display').innerHTML = event.target.innerHTML;
+});
+document.querySelector('.five').addEventListener('click', function(event) {
+    console.log(event.target);
+    document.getElementById('display').innerHTML = event.target.innerHTML;
+});
+document.querySelector('.six').addEventListener('click', function(event) {
+    console.log(event.target);
+    document.getElementById('display').innerHTML = event.target.innerHTML;
+});
+document.querySelector('.seven').addEventListener('click', function(event) {
+    console.log(event.target);
+    document.getElementById('display').innerHTML = event.target.innerHTML;
+});
+document.querySelector('.eight').addEventListener('click', function(event) {
+    console.log(event.target);
+    document.getElementById('display').innerHTML = event.target.innerHTML;
+});
+document.querySelector('.nine').addEventListener('click', function(event) {
+    console.log(event.target);
+    document.getElementById('display').innerHTML = event.target.innerHTML;
+});
+document.querySelector('.zero').addEventListener('click', function(event) {
+    console.log(event.target);
+    document.getElementById('display').innerHTML = event.target.innerHTML;
+});
+document.querySelector('.add').addEventListener('click', function(event) {
+    console.log(event.target);
+    document.getElementById('display').innerHTML = event.target.innerHTML;
+});
+document.querySelector('.sub').addEventListener('click', function(event) {
+    console.log(event.target);
+    document.getElementById('display').innerHTML = event.target.innerHTML;
+});
+document.querySelector('.divide').addEventListener('click', function(event) {
+    console.log(event.target);
+    document.getElementById('display').innerHTML = event.target.innerHTML;
+});
+document.querySelector('.multi').addEventListener('click', function(event) {
+    console.log(event.target);
+    document.getElementById('display').innerHTML = event.target.innerHTML;
+});
+document.querySelector('.equal').addEventListener('click', function(event) {
+    console.log(event.target);
+    document.getElementById('display').innerHTML = event.target.innerHTML;
+});
+document.querySelector('.clear').addEventListener('click', function() {
+    document.querySelector( '.display' ).innerHTML = " ";
+});
+
+
+
+
